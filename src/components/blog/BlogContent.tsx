9f6698c4d80cd0895d46a0d42f502bf470b0e0c5
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-02-03 12:10:10 +0530

    BlogContent

--- src/components/blog/BlogContent.tsx
+++ src/components/blog/BlogContent.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 interface BlogContentProps {
   content: string;
