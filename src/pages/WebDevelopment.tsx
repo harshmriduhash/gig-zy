e16254ffd1a191327daea42764638e1926c1c8f2
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-02-04 13:51:30 +0530

    web

--- src/pages/WebDevelopment.tsx
+++ src/pages/WebDevelopment.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Layout } from '../components/layout/Layout';
-import { WebDevHero } from '../components/webdev/WebDevHero';
-import { WebDevServices } from '../components/webdev/WebDevServices';
-import { WebDevExperts } from '../components/webdev/WebDevExperts';
-import { WebDevProcess } from '../components/webdev/WebDevProcess';
-import { WebDevTestimonials } from '../components/webdev/WebDevTestimonials';
+import React from "react";
+import { Layout } from "../components/layout/Layout";
+import { WebDevHero } from "../components/webdev/WebDevHero";
+import { WebDevServices } from "../components/webdev/WebDevServices";
+import { WebDevExperts } from "../components/webdev/WebDevExperts";
+import { WebDevProcess } from "../components/webdev/WebDevProcess";
+import { WebDevTestimonials } from "../components/webdev/WebDevTestimonials";
 
 export function WebDevelopment() {
   return (
