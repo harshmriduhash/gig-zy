44fdd3362897684bfa36f7da2f71cdce9029df46
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-02-04 13:52:41 +0530

    HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { HeroSection } from '../components/hero/HeroSection';
-import { WhyChooseGigzy } from '../components/features/WhyChooseGigzy';
-import { FeaturedSection } from '../components/features/FeaturedSection';
-import { AINicheServices } from '../components/services/ai-niche/AInicheServices';
-import { GigzyPro } from '../components/GigzyPro';
-import { BlogSection } from '../components/blog/BlogSection';
-import { NewsletterSubscribe } from '../components/newsletter/NewsletterSubscribe';
+import React from "react";
+import { HeroSection } from "../components/hero/HeroSection";
+import { WhyChooseGigzy } from "../components/features/WhyChooseGigzy";
+import { FeaturedSection } from "../components/features/FeaturedSection";
+import { AINicheServices } from "../components/services/ai-niche/AInicheServices";
+import { GigzyPro } from "../components/GigzyPro";
+import { BlogSection } from "../components/blog/BlogSection";
+import { NewsletterSubscribe } from "../components/newsletter/NewsletterSubscribe";
 
 export function HomePage() {
   return (
